de5b3ca452b6f7b9f8a70ca833370bf2c9c8e621
hartley9 <k.hartley1@lancaster.ac.uk>
2017-05-04 14:18:20 +0100

    Added a start screen, altered the speed at which the centipede moves

--- scr.c
+++ scr.c
@@ -3,23 +3,31 @@
 #include <unistd.h>
 #include <string.h>
 
-#define DELAY 30000
+#define DELAY 80000
 
 int main(int argc, char *argv[])
 {
 	int x = 0, y = 0;
+	int delay = 80000;
 	int max_y = 0, max_x = 0;
 	int next_x = 0;
 	
 	int direction = 1;
 	
-	char catPill[] = "ooooo";
+	char catPill[] = "OOOOOOOOOO";
 
 	initscr();
-	cbreak();
 	curs_set(FALSE);
 	
+	//getmaxyx(stdscr, max_y, max_x);
+	//move(max_y/2, max_x/2);
 	
+	printw("Welcome to Centipede!\n");
+	printw("<--Press Any Key to Start-->");
+	getch(); //Waits for user to press a key
+	clear(); //Clears the screen
+	
+	//move(0,0);
 	
 	//printw("It works");
 	while(1)
@@ -37,16 +45,21 @@ int main(int argc, char *argv[])
 		if (next_x >= (max_x - strlen(catPill)) || next_x < 0)
 			{
 			direction *= -1;
+			y++;
 			}
 		else 
 			{
 			x += direction;
 			}
+			
+		cbreak(); //Close window when ctrl + c is pressed
 	}
 	
 	
 	getch();
+	
 	endwin();
 	
 	return 0;
+
 }
