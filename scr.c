bfd9e4e6b6c2fbbfa61ce74e115c0b74a29bfac1
hartley9 <k.hartley1@lancaster.ac.uk>
2017-05-04 15:02:40 +0100

    Added a player ship, and also added colours to the start screen and game screen

--- scr.c
+++ scr.c
@@ -2,8 +2,12 @@
 #include <ncurses.h>
 #include <unistd.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define DELAY 80000
+
+
+
 
 int main(int argc, char *argv[])
 {
@@ -15,27 +19,42 @@ int main(int argc, char *argv[])
 	int direction = 1;
 	
 	char catPill[] = "OOOOOOOOOO";
+	char player[] = "W";
 
 	initscr();
 	curs_set(FALSE);
 	
-	//getmaxyx(stdscr, max_y, max_x);
-	//move(max_y/2, max_x/2);
+	//Color
+	if(has_colors() == FALSE)
+	{
+		endwin();
+		printf("Color is not supported in this terminal\n");
+		exit(1);
+	}
 	
+	
+	start_color();
+	init_pair(1, COLOR_CYAN, COLOR_BLACK);
+	attron(COLOR_PAIR(1));
 	printw("Welcome to Centipede!\n");
 	printw("<--Press Any Key to Start-->");
+	attroff(COLOR_PAIR(1));
 	getch(); //Waits for user to press a key
 	clear(); //Clears the screen
 	
 	//move(0,0);
 	
 	//printw("It works");
+	
+	init_pair(1, COLOR_GREEN, COLOR_BLACK);
+	attron(COLOR_PAIR(1));
 	while(1)
 	{
 		getmaxyx(stdscr, max_y, max_x);
 		
 		clear();
 		mvprintw(y, x, catPill);
+		mvprintw((max_y-(max_y/3)), (max_x/2), player);
 		refresh();
 		
 		usleep(DELAY);
