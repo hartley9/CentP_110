1453b7606fdc03f5e39a98a0057a872ad48a494e
hartley9 <k.hartley1@lancaster.ac.uk>
2017-05-10 15:05:56 +0100

    Added downward movement to player ship, however there is currently a bug where if the player ship reaches the boundaries of the window, any future movement attempted will not occur

--- scr.c
+++ scr.c
@@ -20,7 +20,9 @@ void startScreen(){
 	init_pair(1, COLOR_CYAN, COLOR_BLACK);
 	attron(COLOR_PAIR(1));
 	printw("Welcome to Centipede!\n");
+	printw("Version 1.0.0\n");
 	printw("<--Press Any Key to Start-->");
+	
 	attroff(COLOR_PAIR(1));
 	
 	getch(); //Waits for user to press a key
@@ -28,12 +30,7 @@ void startScreen(){
 	
 	}
 
-void centipede_movement(int cent_x, int cent_y, int delay, int max_x, int max_y,
-						int next_x_centP, int direction, char catPill){
-	
-		
-	
-	}
+
 
 
 int main(int argc, char *argv[])
@@ -43,7 +40,9 @@ int main(int argc, char *argv[])
 	int max_y = 0, max_x = 0;
 	int next_x_centP = 0;
 	int next_x_playr = 0;
-	int direction = 1;
+	int next_y_playr = 0;
+	int cent_direction = 1;
+	int player_direction = 1;
 	char catPill[] = "OOOOOOOOOO";
 	char player[] = "W";
 	
@@ -78,32 +77,52 @@ int main(int argc, char *argv[])
 		
 		usleep(DELAY);
 		
-		next_x_centP = cent_x + direction;
+		next_x_centP = cent_x + cent_direction;
+		
+		next_y_playr = play_y + player_direction;
 		
 		if (next_x_centP >= (max_x - strlen(catPill)) || next_x_centP < 0)
 			{
-			direction *= -1;
+			cent_direction *= -1;
 			cent_y++;
 			}
 		else 
 			{
-			cent_x += direction;
+			cent_x += cent_direction;
 			}
 		
 		//next_x_playr = play
 		//Key presses
-		int playerMove;
-		halfdelay(1); 
+		//int playerMove;
+		halfdelay(2); 
 		int ch;
+		int c;
 		ch = getch();
-		if(ch == KEY_UP)
+		switch(ch)
+		{
+			case KEY_UP:
+				if (next_y_playr >= (max_y - strlen(player)) || next_y_playr < 0)
+				{}
+				else{play_y = play_y - 0.25; player_direction *= -1; addch('x');}
+				break;
+			
+			case KEY_DOWN:
+				if (next_y_playr >= (max_y - strlen(player)) || next_y_playr < 0)
+				{}
+				else{play_y = play_y + 1; player_direction = 1;}
+				break;
+				
+				
+		}
+	
+	/*	if(ch == KEY_UP)
 		{	
 			
 			if (next_x_playr >= (max_y - strlen(player)) || next_x_playr < 0)
 			{}
 			else{play_y = play_y - 0.25;}
 			
-		}
+		}*/
 		
 		clear();
 			
